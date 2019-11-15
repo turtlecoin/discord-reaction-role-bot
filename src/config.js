6329625e0811fef2438ef0b16aeebb5b0f83d5ae
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2019-11-15 17:51:55 +0000

    Turtle-ify config

--- src/config.js
+++ src/config.js
@@ -1,22 +1,16 @@
 module.exports = {
-  token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
-  rules: {
-    '632674590849630219': {
-      channelId: '258314939998011393',
-      isUnique: true,
-      emojiRoleMap: {
-        '🎉': ['542138999511187486']
-      }
-    },
-    '632674552886853653': {
-      channelId: '258314939998011393',
-      isUnique: true,
-      emojiRoleMap: {
-        '\u0031\u20E3': ['349040254600216576'],
-        '\u0032\u20E3': ['349040055525965835'],
-        '\u0033\u20E3': ['349039979219124225'],
-        '\u0034\u20E3': ['349039843998695425']
-      }
+    token: ('TOKEN' in process.env ? process.env : require('../config')).TOKEN,
+    rules: {
+        '628591450153811968': {
+            channelId: '628588108157288459',
+            isUnique: true,
+            emojiRoleMap: {
+                '🐢': ['628588267763138571']
+            },
+            response: {
+                channel: '471023390954618883',
+                content: (user) => `Welcome ${user} to TRTL Network!`,
+            },
+        },
     }
-  }
 };
