2e688b7c5e187f8cc0fe8a202716042f122fd824
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2020-01-30 04:19:39 +0000

    Need to fetch old messages so we can listen for reactions

--- src/handlers/ready.js
+++ src/handlers/ready.js
@@ -1,3 +1,14 @@
 module.exports = (client) => {
   console.log(`${client.user.tag}: Ready`);
+
+  for (const messageId of Object.keys(rules)) {
+    const { channelId } = rules[messageId];
+
+    const channel = await client.channels.fetch(channelId);
+
+    if (channel.type !== 'text') continue;
+
+    const message = await channel.messages.fetch(messageId);
+    if (!message) continue;
+  }
 };
